a663c8820baa974484f4ae8aa65ed602f9567597
Ubuntu <chastindavisngc@chastindavisngc.qevphmmbabge3cflpehl1kwo3c.gx.internal.cloudapp.net>
2021-04-20 01:03:48 +0000

    unzipped files

---
+++ ansible_artifact/src/main/java/com/example/ansible/ansible_artifact/AnsibleArtifactApplication.java
@@ -0,0 +1,13 @@
+package com.example.ansible.ansible_artifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnsibleArtifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnsibleArtifactApplication.class, args);
+	}
+
+}
---
+++ ansible_artifact/src/test/java/com/example/ansible/ansible_artifact/AnsibleArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ansible.ansible_artifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnsibleArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
